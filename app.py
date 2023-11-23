f330eb0cb37ff9af4fa17cb19a6d98b7d77c4b40
Arindam Saha <arindamsaha1507@gmail.com>
2023-11-23 10:21:13 -0500

    pylint compliance

--- app.py
+++ app.py
@@ -1,70 +1,67 @@
 """Streamlit app for reference extraction."""
 
 import streamlit as st
-from reference import (
-    get_references,
-)  # Replace 'your_module' with the actual name of your module
+from reference import get_references, Reference
 
 
-def card(title, author, year, url, citation, abstract):
+def card(ref: Reference):
     """Helper function to display a card"""
-    st.markdown(f"### {title}")
-    st.markdown(f"- [Link]({url}) {author} ({year})")
-    if abstract != "":
+    st.markdown(f"### {ref.title}")
+    st.markdown(f"- [Link]({ref.url}) {ref.author} ({ref.year})")
+    if ref.abstract != "":
         with st.expander("Abstract", expanded=False):
-            st.write(abstract)
-    st.markdown(f"```\n{citation}\n```")
+            st.write(ref.abstract)
+    st.markdown(f"```\n{ref.citation}\n```")
 
 
-# Streamlit page configuration
-st.set_page_config(page_title="Reference Extractor", layout="wide")
+def main():
+    """Main function"""
 
-# Title
-st.title("Pramāṇa: Personal Reference and Article Management Application")
+    # Streamlit page configuration
+    st.set_page_config(page_title="Reference Extractor", layout="wide")
 
-st.markdown(
-    """
-    This app extracts references from a query using the 
-    [Crossref API](https://www.crossref.org/documentation/retrieve-metadata/rest-api/) 
-    and [Habanero](https://github.com/sckott/habanero) client.
+    # Title
+    st.title("Pramāṇa: Personal Reference and Article Management Application")
 
-    Simply enter a search query and click the button to extract references.
-    Click on the copy button on the top right of each box 
-    to copy the BibTeX citation to your clipboard.
+    st.markdown(
+        """
+        This app extracts references from a query using the 
+        [Crossref API](https://www.crossref.org/documentation/retrieve-metadata/rest-api/) 
+        and [Habanero](https://github.com/sckott/habanero) client.
 
-    Note: This app is still in development and may not work as expected. Any feedback is welcome!
-    """
-)
+        Simply enter a search query and click the button to extract references.
+        Click on the copy button on the top right of each box 
+        to copy the BibTeX citation to your clipboard.
 
-# Input fields
-query = st.text_input("Enter your query:", "")
-limit = st.number_input("Limit of references:", min_value=1, max_value=100, value=20)
+        Note: This app is still in development and may not work as expected. 
+        Any feedback is welcome!
+        """
+    )
 
-textbox = st.empty()
+    # Input fields
+    query = st.text_input("Enter your query:", "")
+    limit = st.number_input(
+        "Limit of references:", min_value=1, max_value=100, value=20
+    )
 
-# Button to extract references
-if st.button("Extract References"):
-    if query:
-        # Extract references
-        try:
-            references = get_references(query, limit)
+    # Button to extract references
+    if st.button("Extract References"):
+        if query:
+            # Extract references
+            try:
+                references = get_references(query, limit)
 
-            # Display each reference
-            for ref in references:
-                ref.get_citation()
-                ref.get_abstract()
-                card(
-                    ref.title[0],
-                    ref.authors,
-                    ref.year,
-                    ref.url,
-                    ref.citation,
-                    ref.abstract,
-                )
+                # Display each reference
+                for ref in references:
+                    ref.get_citation()
+                    ref.get_abstract()
+                    card(ref)
 
-        except Exception as e:  # pylint: disable=broad-except
-            st.error(f"An error occurred: {e}")
-    else:
-        st.warning("Please enter a query to extract references.")
+            except Exception as e:  # pylint: disable=broad-except
+                st.error(f"An error occurred: {e}")
+        else:
+            st.warning("Please enter a query to extract references.")
 
-# Additional Streamlit components can be added here as needed
+
+if __name__ == "__main__":
+    main()
